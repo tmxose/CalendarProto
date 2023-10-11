d04e86fbbc8aefaf18c7bdd52024fee626c98478
Shin hyukjoo <tmxose@naver.com>
2023-10-11 17:12:52 +0900

    custumcalendar add event

--- app/src/main/java/com/example/gcalendars/AddEvent.java
+++ app/src/main/java/com/example/gcalendars/AddEvent.java
@@ -3,16 +3,10 @@ package com.example.gcalendars;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -41,40 +35,31 @@ public class AddEvent extends AppCompatActivity {
         Intent intent = getIntent();
         // 선택한 날짜를 받아옵니다.
         selectedDate = intent.getStringExtra("selectedDate");
-        eventDateEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDatePicker();
-            }
-        });
 
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveEvent();
-            }
-        });
+        // 날짜 선택 버튼에 리스너 추가
+        eventDateEditText.setOnClickListener(v -> showDateDialog());
+
+        // 저장 버튼에 리스너 추가
+        saveButton.setOnClickListener(v -> saveEvent());
     }
 
-    private void showDatePicker() {
+    // 날짜 선택 다이얼로그를 띄우는 함수
+    private void showDateDialog() {
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         datePickerDialog = new DatePickerDialog(this,
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDayOfMonth) {
-                        selectedDate = selectedYear + "/" + (selectedMonth + 1) + "/" + selectedDayOfMonth;
-                        eventDateEditText.setText(selectedDate);
-                        datePickerDialog.dismiss(); // 다이얼로그를 닫습니다.
-                    }
+                (view, selectedYear, selectedMonth, selectedDayOfMonth) -> {
+                    selectedDate = selectedYear + "/" + (selectedMonth + 1) + "/" + selectedDayOfMonth;
+                    eventDateEditText.setText(selectedDate);
+                    datePickerDialog.dismiss(); // 다이얼로그를 닫습니다.
                 }, year, month, day);
         datePickerDialog.show();
     }
 
-
+    // 일정을 저장하는 함수
     private void saveEvent() {
         String eventTitle = eventTitleEditText.getText().toString();
         String eventDate = eventDateEditText.getText().toString();
@@ -85,22 +70,15 @@ public class AddEvent extends AppCompatActivity {
             event.put("title", eventTitle);
             event.put("date", eventDate);
             event.put("content", eventContent);
-            // Firebase Database에 Map형식으로 업로드 하는 코드
+
+            // Firebase Firestore에 일정 정보 업로드
             db.collection("events")
                     .add(event)
-                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Toast.makeText(getApplicationContext(), "일정이 추가되었습니다.", Toast.LENGTH_SHORT).show();
-                            finish();
-                        }
+                    .addOnSuccessListener(documentReference -> {
+                        Toast.makeText(getApplicationContext(), "일정이 추가되었습니다.", Toast.LENGTH_SHORT).show();
+                        finish();
                     })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(getApplicationContext(), "일정 추가에 실패했습니다.", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                    .addOnFailureListener(e -> Toast.makeText(getApplicationContext(), "일정 추가에 실패했습니다.", Toast.LENGTH_SHORT).show());
         } else {
             Toast.makeText(this, "일정 제목과 날짜를 입력해주세요.", Toast.LENGTH_SHORT).show();
         }
--- app/src/main/java/com/example/gcalendars/MyCalendar.java
+++ app/src/main/java/com/example/gcalendars/MyCalendar.java
@@ -3,20 +3,16 @@ package com.example.gcalendars;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 public class MyCalendar extends AppCompatActivity {
-    private TextView editTextDate;
+    private TextView editTextDate; // 선택한 날짜를 표시하는 TextView
     private final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy MM dd", Locale.getDefault());
 
     @Override
@@ -24,40 +20,32 @@ public class MyCalendar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_my);
 
-        editTextDate = findViewById(R.id.editTextDate);
-        Button addButton = findViewById(R.id.buttonAdd);
-        CalendarView calendarView = findViewById(R.id.calendarView);
+        editTextDate = findViewById(R.id.editTextDate); // XML 레이아웃에서 해당 TextView 찾기
+        Button addButton = findViewById(R.id.buttonAdd); // "일정 추가" 버튼
+        CalendarView calendarView = findViewById(R.id.calendarView); // CalendarView
 
         // 캘린더 뷰의 날짜 선택 이벤트 처리
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                Calendar selectedCalendar = Calendar.getInstance();
-                selectedCalendar.set(year, month, dayOfMonth);
-                String selectedDate = dateFormatter.format(selectedCalendar.getTime());
-                editTextDate.setText(selectedDate);
-            }
+        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            Calendar selectedCalendar = Calendar.getInstance();
+            selectedCalendar.set(year, month, dayOfMonth);
+            String selectedDate = dateFormatter.format(selectedCalendar.getTime());
+            editTextDate.setText(selectedDate); // 선택한 날짜를 TextView에 표시
         });
 
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String selectedDate = editTextDate.getText().toString();
-                // 선택한 날짜를 인텐트에 추가
-                Intent intent = new Intent(MyCalendar.this, AddEvent.class);
-                intent.putExtra("selectedDate", selectedDate);
-                startActivity(intent);
-            }
+        addButton.setOnClickListener(v -> {
+            String selectedDate = editTextDate.getText().toString();
+            // 선택한 날짜를 인텐트에 추가
+            Intent intent = new Intent(MyCalendar.this, AddEvent.class);
+            intent.putExtra("selectedDate", selectedDate);
+            startActivity(intent); // AddEvent 액티비티 시작
         });
 
-        editTextDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDatePicker();
-            }
+        editTextDate.setOnClickListener(v -> {
+            showDatePicker(); // 날짜 선택 다이얼로그 표시
         });
     }
-    // 선택한 날짜 박스에 보여 주는 함수
+
+    // 선택한 날짜를 다이얼로그에서 보여주는 함수
     private void showDatePicker() {
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
@@ -65,14 +53,11 @@ public class MyCalendar extends AppCompatActivity {
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(android.widget.DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        String selectedDate = year + "년 " + (monthOfYear + 1) + "월 " + dayOfMonth + "일";
-                        editTextDate.setText(selectedDate);
-                    }
+                (view, year1, monthOfYear, dayOfMonth) -> {
+                    String selectedDate = year1 + "년 " + (monthOfYear + 1) + "월 " + dayOfMonth + "일";
+                    editTextDate.setText(selectedDate); // 선택한 날짜를 TextView에 표시
                 }, year, month, day);
 
-        datePickerDialog.show();
+        datePickerDialog.show(); // 날짜 선택 다이얼로그 표시
     }
 }
