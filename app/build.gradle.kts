8a829cb1914299264f9203e137f6c02221775d86
Shin hyukjoo <tmxose@naver.com>
2023-10-22 21:52:45 +0900

    bug fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,10 +35,11 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
     implementation("com.google.android.material:material:1.10.0")
     implementation("com.google.firebase:firebase-firestore:24.9.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation ("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
